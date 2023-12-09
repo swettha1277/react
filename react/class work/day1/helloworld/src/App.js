16ed93d2bfde0dc866f2388994f19c4e73b47e94
swettha1277 <153355744+swettha1277@users.noreply.github.com>
2023-12-09 14:40:36 +0530

    This repositary consists of reactjs files

---
+++ react/class work/day1/helloworld/src/App.js
@@ -0,0 +1,13 @@
+import Sample from './sample.js'
+import './App.js'
+
+function App()
+{
+  return(
+    <div>
+      <Sample/>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+export default App
---
+++ react/class work/day1/helloworld/src/sample.js
@@ -0,0 +1,9 @@
+function sample()
+{
+    return(
+        <div>
+            <p>This is a sample component</p>
+        </div>
+    );
+}
+export default sample
